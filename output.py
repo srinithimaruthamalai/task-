c9d97fb3698c8339a26bfc3a8e2d565d2d0fc6c0
srinithimaruthamalai <srinithimaruthamalai@gmail.com>
2026-09-07 13:45:07 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,159 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+(a+b)^2= 25
+(a-b)^2= 1
+a^2-b^2= -5
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+(a+b)^2= 25
+(a-b)^2= 1
+a^2-b^2= -5
+After swapping:
+a= 3
+b= 2
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+(a+b)^2= 25
+(a-b)^2= 1
+a^2-b^2= -5
+After swapping:
+a= 3
+b= 2
+After swapping:
+a= 3
+b= 2
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+(a+b)^2= 25
+(a-b)^2= 1
+a^2-b^2= -5
+After swapping:
+a= 3
+b= 2
+After swapping:
+a= 3
+b= 2
+miles= 6.3
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+(a+b)^2= 25
+(a-b)^2= 1
+a^2-b^2= -5
+After swapping:
+a= 3
+b= 2
+After swapping:
+a= 3
+b= 2
+miles= 6.3
+fahrenheit= 212.0
+
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+(a+b)^2= 25
+(a-b)^2= 1
+a^2-b^2= -5
+After swapping:
+a= 3
+b= 2
+After swapping:
+a= 3
+b= 2
+miles= 6.3
+fahrenheit= 212.0
+last digit= 5
+last digit= 35
+>>> 
+===== RESTART: C:/Users/ELCOT/OneDrive/Desktop/srinithi/python/input.py =====
+Hello
+30
+10
+200
+2.0
+0
+square root= 10.0
+Area of the triangle= 25.0
+(a+b)^2= 25
+(a-b)^2= 1
+a^2-b^2= -5
+After swapping:
+a= 3
+b= 2
+After swapping:
+a= 3
+b= 2
+miles= 6.3
+fahrenheit= 212.0
+last digit= 5
+last digit= 35
+number= 54623
+middle digit= 6
+square= 36
